3bf21a5f4424416e4d8c752d08016bb13f47c403
huangyoucheng <huangyoucheng@baidu.com>
2016-01-07 19:25:10 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by baidu on 16/1/7.
+//  Copyright © 2016年 yockie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
